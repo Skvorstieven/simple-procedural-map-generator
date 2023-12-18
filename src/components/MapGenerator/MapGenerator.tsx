e70c841e048849b6cd29c8c38d3cc8bfa00de085
Stepan <skvorstieven@yandex.ru>
2023-12-18 16:07:04 +0300

    feat: setup MapGenerator component and add envmap

--- src/components/MapGenerator/MapGenerator.tsx
+++ src/components/MapGenerator/MapGenerator.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Environment } from "@react-three/drei";
+
+
+function Water() {
+  const waterRef = useRef<THREE.Mesh>(null!);
+
+  useFrame(() => {
+    if (!waterRef.current) {
+      return;
+    }
+
+    waterRef.current.rotation.x += 0.01;
+    waterRef.current.rotation.y += 0.01;
+  })
+
+  return (
+    <mesh ref={waterRef}>
+      <boxGeometry args={[2, 2, 2]} />
+      <meshPhysicalMaterial />
+    </mesh>
+  )
+}
 
 export default function MapGenerator() {
   return (
-    <div className="map-generator" />
+    <Canvas>
+      <Environment preset="dawn" />
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Water />
+    </Canvas>
   );
 }
