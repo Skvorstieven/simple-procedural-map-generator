1eea1550317999a3558f5f9f29510dffa908c61b
Stepan <skvorstieven@yandex.ru>
2023-12-19 11:32:15 +0300

    refactor: move cylinder size controls to MG

--- src/components/HexGrid/HexGrid.tsx
+++ src/components/HexGrid/HexGrid.tsx
@@ -2,14 +2,17 @@ import { Vector2 } from "three";
 import { createNoise2D } from 'simplex-noise';
 import { Cylinder, } from "@react-three/drei";
 
-export default function HexGrid() {
+export default function HexGrid(props: { size: number, waterHeight: number, heightScale: number }) {
+
+  const { size, waterHeight, heightScale } = props;
 
   function Hexagon(props: { x: number, y: number, height: number }) {
     const { x, y, height } = props;
+    const cylinderHeight = height === waterHeight ? height-0.1 : height;
     return (
       <mesh castShadow receiveShadow>
-        <Cylinder args={[1, 1, height, 6, 1, false]} position={[x, height*0.5, y]}>
-          <meshStandardMaterial flatShading />
+        <Cylinder args={[1, 1, cylinderHeight, 6, 1, false]} position={[x, height*0.5, y]}>
+          <meshStandardMaterial flatShading color={0xadadad}/>
         </Cylinder>
       </mesh>
     )
@@ -23,13 +26,13 @@ export default function HexGrid() {
 
   function getHexesData() {
     const data = [];
-    for(let x = -20; x <= 20; x++) {
-      for(let y = -20; y <= 20; y++) {
+    for(let x = -size; x <= size; x++) {
+      for(let y = -size; y <= size; y++) {
         const position = convertTileToPosition(x, y);
   
-        if(position.length() > 18) continue;
+        if(position.length() > size-2) continue;
   
-        const noise = (createNoise(x*0.1, y*0.1) + 1)*0.5;
+        const noise = (createNoise(x*0.1, y*0.1) + 1)*heightScale;
         const height = Math.pow(noise, 1.5)*10;
   
         data.push({position, height});
