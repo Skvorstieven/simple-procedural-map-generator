74f35f3023e3ab4d1571339bd663202857127eac
Stepan <skvorstieven@yandex.ru>
2023-12-24 16:43:21 +0300

    fix: calibrate water height slider

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -12,14 +12,14 @@ export default function App() {
   // Use Leva controls to manage configuration settings
   const config = useControls({
     size: {value: 100, min: 1, max: 500, step: 1}, // Size of the map
-    waterHeight: {value: 5, min: 0, max: 10, step: 1}, // Height of the water surface
+    waterHeight: {value: 5, min: 0, max: 30, step: 1}, // Height of the water surface
     heightScale: {value: 1, min: 0.1, max: 2, step: 0.05}, // Scale for terrain height
     noiseScale: {value: 6, min: 1, max: 100, step: 1}, // Scale for the noise function
     hexColor: '#adadad',
     waterColor: '#0cc48d',
     ambienLightColor: '#f0cd00',
     autoRotate: false // Whether to enable auto-rotation
-  })
+  });
 
   return (
     <>
