6e542cdd576d69899359f8c7bacef66e198103c5
Stepan <skvorstieven@yandex.ru>
2023-12-25 10:43:31 +0300

    feat: add different biomes

--- src/components/HexGrid/HexGrid.tsx
+++ src/components/HexGrid/HexGrid.tsx
@@ -1,52 +1,51 @@
 import * as THREE from 'three';
 import { extend } from '@react-three/fiber';
+import createInstancedMesh from '../../utils/createInstancedMesh';
 
 // Extend the mesh class with the instanced mesh class
 extend({ InstancedMesh: THREE.InstancedMesh });
 
-// Dunction for height correction near water surface to avoid visual glitch
-function heightCorrection(height: number, waterHeight: number) {
-  if(height <= waterHeight+0.2 && height >= waterHeight) {
-    return height + 0.1;
-  }
-  else if(height >= waterHeight-0.2 && height < waterHeight) {
-    return height - 0.1;
-  }
-  else {
-    return height;
-  }
-}
-
 export default function HexGrid(props: { hexesData: { position: { x: number; y: number }; height: number }[]; waterHeight: number; color: THREE.Color }) {
-  const { hexesData, waterHeight, color } = props;
+  const { hexesData, waterHeight, /* color */ } = props;
+
+  const sandHexes = hexesData.filter(({ height }) => height < waterHeight+1);
+  const plainHexes = hexesData.filter(({ height }) => height >= waterHeight+1 && height < waterHeight+5);
+  const forestHexes = hexesData.filter(({ height }) => height >= waterHeight+5 && height < waterHeight+15);
+  const stoneHexes = hexesData.filter(({ height }) => height >= waterHeight+15 && height < waterHeight+20);
+  const snowHexes = hexesData.filter(({ height }) => height >= waterHeight+20);
 
   // Create a buffer geometry for the hexagon
   const hexagonGeometry = new THREE.CylinderGeometry(1, 1, 1, 6, 1, false);
 
   // Create a material for the instanced mesh
-  const hexagonMaterial = new THREE.MeshStandardMaterial({ flatShading: true, color });
+  /* const hexagonMaterial = new THREE.MeshStandardMaterial({ flatShading: true, color }); */
+  const sandHexagonMaterial = new THREE.MeshStandardMaterial({ flatShading: true, color: new THREE.Color('sandybrown') });
+  const plainHexagonMaterial = new THREE.MeshStandardMaterial({ flatShading: true, color: new THREE.Color('green') });
+  const forestHexagonMaterial = new THREE.MeshStandardMaterial({ flatShading: true, color: new THREE.Color('darkgreen') });
+  const stoneHexagonMaterial = new THREE.MeshStandardMaterial({ flatShading: true, color: new THREE.Color('gray') });
+  const snowHexagonMaterial = new THREE.MeshStandardMaterial({ flatShading: true, color: new THREE.Color('white') });
 
   // Create an instanced mesh
-  const hexagons = new THREE.InstancedMesh(hexagonGeometry, hexagonMaterial, hexesData.length);
-
-  // Set the position and scale for each instance
-  hexesData.forEach(({ position, height }, index) => {
-    const hexagonHeight = heightCorrection(height, waterHeight);
-    
-    // Create a matrix for each instance
-    const matrix = new THREE.Matrix4()
-      .compose(
-        new THREE.Vector3(position.x, hexagonHeight * 0.5, position.y), // Set position
-        new THREE.Quaternion(),
-        new THREE.Vector3(1, 1, 1)
-      )
-      .scale(new THREE.Vector3(1, hexagonHeight, 1)); // Scale based on hexagonHeight
-
-    hexagons.setMatrixAt(index, matrix);
-  });
+  const sandHexagons = createInstancedMesh(hexagonGeometry, sandHexagonMaterial, hexesData.length, sandHexes, waterHeight);
+  const plainHexagons = createInstancedMesh(hexagonGeometry, plainHexagonMaterial, hexesData.length, plainHexes, waterHeight);
+  const forestHexagons = createInstancedMesh(hexagonGeometry, forestHexagonMaterial, hexesData.length, forestHexes, waterHeight);
+  const stoneHexagons = createInstancedMesh(hexagonGeometry, stoneHexagonMaterial, hexesData.length, stoneHexes, waterHeight);
+  const snowHexagons = createInstancedMesh(hexagonGeometry, snowHexagonMaterial, hexesData.length, snowHexes, waterHeight);
 
   // Tell Three.js to update the instance matrices
-  hexagons.instanceMatrix.needsUpdate = true;
+  sandHexagons.instanceMatrix.needsUpdate = true;
+  plainHexagons.instanceMatrix.needsUpdate = true;
+  forestHexagons.instanceMatrix.needsUpdate = true;
+  stoneHexagons.instanceMatrix.needsUpdate = true;
+  snowHexagons.instanceMatrix.needsUpdate = true;
 
-  return <primitive object={hexagons} />;
+  return (
+    <>
+      <primitive object={sandHexagons} />
+      <primitive object={plainHexagons} />
+      <primitive object={forestHexagons} />
+      <primitive object={stoneHexagons} />
+      <primitive object={snowHexagons} />
+    </>
+  );
 }
